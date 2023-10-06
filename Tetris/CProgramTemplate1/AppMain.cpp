e68630bcd859785862aac126c3ff7bfe8e6bca6c
Soulless5141 <138739109+Soulless5141@users.noreply.github.com>
2023-10-06 12:05:33 +0900

    hを書いた+メインapp修正
    コメントの書き方を修正。
    ヘッダーを書いた。

--- Tetris/CProgramTemplate1/AppMain.cpp
+++ Tetris/CProgramTemplate1/AppMain.cpp
@@ -1,21 +1,34 @@
+/*********************************
+* プログラミング実習(テトリス編)
+*********************************/
 #include"DxLib.h"
 #include"FreamControl.h"
 #include"SceneManager.h"
 #include"InputControl.h"
 
-//マクロ定義
+/******************
+* マクロ定義
+******************/
 #define SCREEN_HEIGHT (720)
 #define SCREEN_WIDTH (1280)
 #define SCREEN_COLORBIT (32)
 #define FONT_SIZE (20)
 
-//型定義
+/***********************
+* 型定義
+***********************/
 
-//グローバル変数宣言
+/***********************
+* グローバル宣言
+***********************/
 
-//プロトタイプ宣言
+/***********************
+* プロトタイプ宣言
+***********************/
 
-//プログラムの開始
+/***********************
+* プログラムの開始
+***********************/
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR ipCmdLine, _In_ int nShowCmd)
 {
